packethost/packet-ccm

add annotation for private network
Signed-off-by: Avi Deitcher <avi@deitcher.net>

main.go
@@ -23,21 +23,22 @@ import (
 )
 
 const (
-	apiKeyName                   = "METAL_API_KEY"
-	projectIDName                = "METAL_PROJECT_ID"
-	facilityName                 = "METAL_FACILITY_NAME"
-	loadBalancerSettingName      = "METAL_LOAD_BALANCER"
-	envVarLocalASN               = "METAL_LOCAL_ASN"
-	envVarBGPPass                = "METAL_BGP_PASS"
-	envVarAnnotationLocalASN     = "METAL_ANNOTATION_LOCAL_ASN"
-	envVarAnnotationPeerASNs     = "METAL_ANNOTATION_PEER_ASNS"
-	envVarAnnotationPeerIPs      = "METAL_ANNOTATION_PEER_IPS"
-	envVarAnnotationSrcIP        = "METAL_ANNOTATION_SRC_IP"
-	envVarAnnotationBGPPass      = "METAL_ANNOTATION_BGP_PASS"
-	envVarEIPTag                 = "METAL_EIP_TAG"
-	envVarAPIServerPort          = "METAL_API_SERVER_PORT"
-	envVarBGPNodeSelector        = "METAL_BGP_NODE_SELECTOR"
-	defaultLoadBalancerConfigMap = "metallb-system:config"
+	apiKeyName                         = "METAL_API_KEY"
+	projectIDName                      = "METAL_PROJECT_ID"
+	facilityName                       = "METAL_FACILITY_NAME"
+	loadBalancerSettingName            = "METAL_LOAD_BALANCER"
+	envVarLocalASN                     = "METAL_LOCAL_ASN"
+	envVarBGPPass                      = "METAL_BGP_PASS"
+	envVarAnnotationLocalASN           = "METAL_ANNOTATION_LOCAL_ASN"
+	envVarAnnotationPeerASNs           = "METAL_ANNOTATION_PEER_ASNS"
+	envVarAnnotationPeerIPs            = "METAL_ANNOTATION_PEER_IPS"
+	envVarAnnotationSrcIP              = "METAL_ANNOTATION_SRC_IP"
+	envVarAnnotationBGPPass            = "METAL_ANNOTATION_BGP_PASS"
+	envVarAnnotationIPv4NetworkPrivate = "METAL_ANNOTATION_NETWORK_IPV4_PRIVATE"
+	envVarEIPTag                       = "METAL_EIP_TAG"
+	envVarAPIServerPort                = "METAL_API_SERVER_PORT"
+	envVarBGPNodeSelector              = "METAL_BGP_NODE_SELECTOR"
+	defaultLoadBalancerConfigMap       = "metallb-system:config"
 )
 
 var (
@@ -189,6 +190,12 @@ func getMetalConfig(providerConfig string) (metal.Config, error) {
 	annotationBGPPass := os.Getenv(envVarAnnotationBGPPass)
 	if annotationBGPPass != "" {
 		config.AnnotationBGPPass = annotationBGPPass
+	}
+
+	config.AnnotationNetworkIPv4Private = metal.DefaultAnnotationNetworkIPv4Private
+	annotationNetworkIPv4Private := os.Getenv(envVarAnnotationIPv4NetworkPrivate)
+	if annotationNetworkIPv4Private != "" {
+		config.AnnotationNetworkIPv4Private = annotationNetworkIPv4Private
 	}
 
 	if rawConfig.EIPTag != "" {

metal/devices.go
@@ -2,6 +2,7 @@ package metal
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -17,12 +18,14 @@ import (
 )
 
 type instances struct {
-	client  *packngo.Client
-	project string
+	client            *packngo.Client
+	k8sclient         kubernetes.Interface
+	project           string
+	annotationNetwork string
 }
 
-func newInstances(client *packngo.Client, projectID string) *instances {
-	return &instances{client, projectID}
+func newInstances(client *packngo.Client, project, annotationNetwork string) *instances {
+	return &instances{client: client, project: project, annotationNetwork: annotationNetwork}
 }
 
 // cloudService implementation
@@ -30,10 +33,11 @@ func (i *instances) name() string {
 	return "instances"
 }
 func (i *instances) init(k8sclient kubernetes.Interface) error {
+	i.k8sclient = k8sclient
 	return nil
 }
 func (i *instances) nodeReconciler() nodeReconciler {
-	return nil
+	return i.reconcileNodes
 }
 func (i *instances) serviceReconciler() serviceReconciler {
 	return nil
@@ -251,4 +255,85 @@ func (i *instances) deviceFromProviderID(providerID string) (*packngo.Device, er
 	}
 
 	return deviceByID(i.client, id)
+}
+
+// reconcileNodes ensures each node has the annotations it needs
+func (i *instances) reconcileNodes(ctx context.Context, nodes []*v1.Node, mode UpdateMode) error {
+	nodeNames := []string{}
+	for _, node := range nodes {
+		nodeNames = append(nodeNames, node.Name)
+	}
+	klog.V(2).Infof("devices.reconcileNodes(): called for nodes %v", nodeNames)
+	// whether adding or syncing, we just enable bgp. Nothing to do when we remove.
+	switch mode {
+	case ModeAdd, ModeSync:
+		for _, node := range nodes {
+			klog.V(2).Infof("instances.reconcileNodes(): add node %s", node.Name)
+			// get the node provider ID
+			id := node.Spec.ProviderID
+			if id == "" {
+				return fmt.Errorf("no provider ID given")
+			}
+
+			// add annotations
+			klog.V(2).Infof("instances.reconcileNodes(): setting annotations on node %s", node.Name)
+			// get the network info
+			network, err := getNodePrivateNetwork(id, i.client)
+			if err != nil || network == "" {
+				klog.Errorf("instances.reconcileNodes(): could not get private network info for node %s: %v", node.Name, err)
+			} else {
+				newAnnotations := make(map[string]string)
+				oldAnnotations := node.Annotations
+				if oldAnnotations == nil {
+					oldAnnotations = make(map[string]string)
+				}
+				val, ok := oldAnnotations[i.annotationNetwork]
+				if !ok || val != network {
+					newAnnotations[i.annotationNetwork] = network
+				}
+
+				// patch the node with the new annotations
+				if len(newAnnotations) > 0 {
+					mergePatch, _ := json.Marshal(map[string]interface{}{
+						"metadata": map[string]interface{}{
+							"annotations": newAnnotations,
+						},
+					})
+
+					if err := patchUpdatedNode(ctx, node.Name, mergePatch, i.k8sclient); err != nil {
+						klog.Errorf("instances.reconcileNodes(): failed to save updated node with annotations %s: %v", node.Name, err)
+					} else {
+						klog.V(2).Infof("instances.reconcileNodes(): annotations set on node %s", node.Name)
+					}
+				} else {
+					klog.V(2).Infof("instances.reconcileNodes(): no change to annotations for %s", node.Name)
+				}
+			}
+		}
+	case ModeRemove:
+		klog.V(2).Info("instances.reconcileNodes(): nothing to do for removing nodes")
+	}
+	klog.V(2).Info("instamces.reconcileNodes(): complete")
+	return nil
+}
+
+// getNodePrivateNetwork use the Equinix Metal API to get the CIDR of the private network given a providerID.
+func getNodePrivateNetwork(deviceID string, client *packngo.Client) (string, error) {
+	device, _, err := client.Devices.Get(deviceID, &packngo.GetOptions{Includes: []string{"ip_addresses.parent_block,parent_block"}})
+
+	if err != nil {
+		return "", err
+	}
+	for _, net := range device.Network {
+		// we only want the private, management, ipv4 network
+		if net.Public || !net.Management || net.AddressFamily != 4 {
+			continue
+		}
+		parent := net.ParentBlock
+		if parent == nil || parent.Network == "" || parent.CIDR == 0 {
+			return "", fmt.Errorf("no network information provided for private address %s", net.String())
+		}
+		return fmt.Sprintf("%s/%d", parent.Network, parent.CIDR), nil
+	}
+	return "", nil
 }

kubernetes-sigs/cloud-provider-equinix-metal

main.go
@@ -23,21 +23,22 @@ import (
 )
 
 const (
-	apiKeyName                   = "METAL_API_KEY"
-	projectIDName                = "METAL_PROJECT_ID"
-	facilityName                 = "METAL_FACILITY_NAME"
-	loadBalancerSettingName      = "METAL_LOAD_BALANCER"
-	envVarLocalASN               = "METAL_LOCAL_ASN"
-	envVarBGPPass                = "METAL_BGP_PASS"
-	envVarAnnotationLocalASN     = "METAL_ANNOTATION_LOCAL_ASN"
-	envVarAnnotationPeerASNs     = "METAL_ANNOTATION_PEER_ASNS"
-	envVarAnnotationPeerIPs      = "METAL_ANNOTATION_PEER_IPS"
-	envVarAnnotationSrcIP        = "METAL_ANNOTATION_SRC_IP"
-	envVarAnnotationBGPPass      = "METAL_ANNOTATION_BGP_PASS"
-	envVarEIPTag                 = "METAL_EIP_TAG"
-	envVarAPIServerPort          = "METAL_API_SERVER_PORT"
-	envVarBGPNodeSelector        = "METAL_BGP_NODE_SELECTOR"
-	defaultLoadBalancerConfigMap = "metallb-system:config"
+	apiKeyName                         = "METAL_API_KEY"
+	projectIDName                      = "METAL_PROJECT_ID"
+	facilityName                       = "METAL_FACILITY_NAME"
+	loadBalancerSettingName            = "METAL_LOAD_BALANCER"
+	envVarLocalASN                     = "METAL_LOCAL_ASN"
+	envVarBGPPass                      = "METAL_BGP_PASS"
+	envVarAnnotationLocalASN           = "METAL_ANNOTATION_LOCAL_ASN"
+	envVarAnnotationPeerASNs           = "METAL_ANNOTATION_PEER_ASNS"
+	envVarAnnotationPeerIPs            = "METAL_ANNOTATION_PEER_IPS"
+	envVarAnnotationSrcIP              = "METAL_ANNOTATION_SRC_IP"
+	envVarAnnotationBGPPass            = "METAL_ANNOTATION_BGP_PASS"
+	envVarAnnotationNetworkIPv4Private = "METAL_ANNOTATION_NETWORK_IPV4_PRIVATE"
+	envVarEIPTag                       = "METAL_EIP_TAG"
+	envVarAPIServerPort                = "METAL_API_SERVER_PORT"
+	envVarBGPNodeSelector              = "METAL_BGP_NODE_SELECTOR"
+	defaultLoadBalancerConfigMap       = "metallb-system:config"
 )
 
 var (
@@ -189,6 +190,12 @@ func getMetalConfig(providerConfig string) (metal.Config, error) {
 	annotationBGPPass := os.Getenv(envVarAnnotationBGPPass)
 	if annotationBGPPass != "" {
 		config.AnnotationBGPPass = annotationBGPPass
+	}
+
+	config.AnnotationNetworkIPv4Private = metal.DefaultAnnotationNetworkIPv4Private
+	annotationNetworkIPv4Private := os.Getenv(envVarAnnotationNetworkIPv4Private)
+	if annotationNetworkIPv4Private != "" {
+		config.AnnotationNetworkIPv4Private = annotationNetworkIPv4Private
 	}
 
 	if rawConfig.EIPTag != "" {

metal/devices.go
@@ -2,6 +2,7 @@ package metal
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"strings"
 
@@ -17,12 +18,14 @@ import (
 )
 
 type instances struct {
-	client  *packngo.Client
-	project string
+	client            *packngo.Client
+	k8sclient         kubernetes.Interface
+	project           string
+	annotationNetwork string
 }
 
-func newInstances(client *packngo.Client, projectID string) *instances {
-	return &instances{client, projectID}
+func newInstances(client *packngo.Client, project, annotationNetwork string) *instances {
+	return &instances{client: client, project: project, annotationNetwork: annotationNetwork}
 }
 
 // cloudService implementation
@@ -30,10 +33,11 @@ func (i *instances) name() string {
 	return "instances"
 }
 func (i *instances) init(k8sclient kubernetes.Interface) error {
+	i.k8sclient = k8sclient
 	return nil
 }
 func (i *instances) nodeReconciler() nodeReconciler {
-	return nil
+	return i.reconcileNodes
 }
 func (i *instances) serviceReconciler() serviceReconciler {
 	return nil
@@ -251,4 +255,85 @@ func (i *instances) deviceFromProviderID(providerID string) (*packngo.Device, er
 	}
 
 	return deviceByID(i.client, id)
+}
+
+// reconcileNodes ensures each node has the annotations it needs
+func (i *instances) reconcileNodes(ctx context.Context, nodes []*v1.Node, mode UpdateMode) error {
+	nodeNames := []string{}
+	for _, node := range nodes {
+		nodeNames = append(nodeNames, node.Name)
+	}
+	klog.V(2).Infof("devices.reconcileNodes(): called for nodes %v", nodeNames)
+	// whether adding or syncing, we just enable bgp. Nothing to do when we remove.
+	switch mode {
+	case ModeAdd, ModeSync:
+		for _, node := range nodes {
+			klog.V(2).Infof("instances.reconcileNodes(): add node %s", node.Name)
+			// get the node provider ID
+			id := node.Spec.ProviderID
+			if id == "" {
+				return fmt.Errorf("no provider ID given")
+			}
+
+			// add annotations
+			klog.V(2).Infof("instances.reconcileNodes(): setting annotations on node %s", node.Name)
+			// get the network info
+			network, err := getNodePrivateNetwork(id, i.client)
+			if err != nil || network == "" {
+				klog.Errorf("instances.reconcileNodes(): could not get private network info for node %s: %v", node.Name, err)
+			} else {
+				newAnnotations := make(map[string]string)
+				oldAnnotations := node.Annotations
+				if oldAnnotations == nil {
+					oldAnnotations = make(map[string]string)
+				}
+				val, ok := oldAnnotations[i.annotationNetwork]
+				if !ok || val != network {
+					newAnnotations[i.annotationNetwork] = network
+				}
+
+				// patch the node with the new annotations
+				if len(newAnnotations) > 0 {
+					mergePatch, _ := json.Marshal(map[string]interface{}{
+						"metadata": map[string]interface{}{
+							"annotations": newAnnotations,
+						},
+					})
+
+					if err := patchUpdatedNode(ctx, node.Name, mergePatch, i.k8sclient); err != nil {
+						klog.Errorf("instances.reconcileNodes(): failed to save updated node with annotations %s: %v", node.Name, err)
+					} else {
+						klog.V(2).Infof("instances.reconcileNodes(): annotations set on node %s", node.Name)
+					}
+				} else {
+					klog.V(2).Infof("instances.reconcileNodes(): no change to annotations for %s", node.Name)
+				}
+			}
+		}
+	case ModeRemove:
+		klog.V(2).Info("instances.reconcileNodes(): nothing to do for removing nodes")
+	}
+	klog.V(2).Info("instances.reconcileNodes(): complete")
+	return nil
+}
+
+// getNodePrivateNetwork use the Equinix Metal API to get the CIDR of the private network given a providerID.
+func getNodePrivateNetwork(deviceID string, client *packngo.Client) (string, error) {
+	device, _, err := client.Devices.Get(deviceID, &packngo.GetOptions{Includes: []string{"ip_addresses.parent_block,parent_block"}})
+
+	if err != nil {
+		return "", err
+	}
+	for _, net := range device.Network {
+		// we only want the private, management, ipv4 network
+		if net.Public || !net.Management || net.AddressFamily != 4 {
+			continue
+		}
+		parent := net.ParentBlock
+		if parent == nil || parent.Network == "" || parent.CIDR == 0 {
+			return "", fmt.Errorf("no network information provided for private address %s", net.String())
+		}
+		return fmt.Sprintf("%s/%d", parent.Network, parent.CIDR), nil
+	}
+	return "", nil
 }